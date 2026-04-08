52acefdeb5a3daaf908e3973bdfcde2abc8cddfc
PepeSiluro <joxemipowers@gmail.com>
2026-04-08 17:52:21 +0200

    Update items.h

--- include/items.h
+++ include/items.h
@@ -3,12 +3,12 @@
 
 #include <stdio.h>
 
-#define MAX_NOMBRE 16
+#define MAX_NOMBRE_ITEM 16
 #define MAX_DESC_I 51
 
 typedef struct {
     char id[5];                 // OB01
-    char nombre[MAX_NOMBRE];
+    char nombre[MAX_NOMBRE_ITEM];
     char desc[MAX_DESC_I];
     int localizacion;           // id sala o -1 inventario
 } t_item;
